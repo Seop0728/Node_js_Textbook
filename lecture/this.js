53c4e44293ccaf9012e9f2d99be3b50cac1514e7
Seop0728 <ms_sh0728@naver.com>
2023-01-20 12:13:27 +0900

    FEAT : this

---
+++ lecture/this.js
@@ -0,0 +1,11 @@
+console.log(this);  // {}
+console.log(this === module.exports);// true
+console.log(this === exports);// true
+
+function whatIsThis() {
+	console.log('function', this === exports, this === global);
+}
+whatIsThis()  // fucntion false true
+
+
+
