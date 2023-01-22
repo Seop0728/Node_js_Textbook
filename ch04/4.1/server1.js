c9924e58b8d83ef38645931e46f678044ebc5068
Seop0728 <ms_sh0728@naver.com>
2023-01-22 14:29:28 +0900

    FEAT : http server

---
+++ ch04/4.1/server1.js
@@ -0,0 +1,16 @@
+const http = require ('http');
+
+const server = http.createServer (( req, res ) => {
+	res.write ('<h1>Hello node</h1>');
+	res.write ('<p>Hello Server</p>');
+	res.end ('<p>Hello Seop</p>');
+}).listen (8080);
+
+server.on ('listening', () => {
+	console.log ('8080포트에서 서버 대기중 입니다');
+	console.log ('http://localhost:8080/');
+});
+
+server.on ('error', ( err ) => {
+	console.log (err);
+});
