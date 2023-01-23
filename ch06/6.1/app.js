1e74571aa57b3eea95dc400878c5bd8056e0e430
Seop0728 <ms_sh0728@naver.com>
2023-01-23 15:18:46 +0900

    update : middleware

--- ch06/6.1/app.js
+++ ch06/6.1/app.js
@@ -1,22 +1,37 @@
-const express = require('express')
-const path = require('path')
-const app = express();
+const express = require ('express');
+const path = require ('path');
+const app = express ();
 
-app.set('port', process.env.PORT || 3000);
+app.set ('port', process.env.PORT || 3000);
 
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname,'index.html'))
+app.use (( req, res, next ) => {
+	console.log ('1 요청을 실행하고싶어요');
+	next ();
+});
+
+app.get ('/', ( req, res ) => {
+	// res.sendFile (path.join (__dirname, 'index.html'));
+	res.status(200).send("hello")
+});
+
+
+app.post ('/', ( req, res ) => {
+	res.send ('hello express');
+});
+
+app.get ('/about', ( req, res ) => {
+	res.send ('hello express');
+});
+
+app.use((req, res, next) => {
+	res.send('404에러')
 })
 
-app.post('/', (req, res) => {
-	res.send('hello express')
-})
+app.use (( err, req, res, next ) => {
+	console.log (err);
+	res.send ("에러 메세지 ");
+});
 
-app.get('/about', (req, res) => {
-	res.send('hello express')
-})
-
-
-app.listen(app.get('port'), () => {
+app.listen (app.get ('port'), () => {
 	console.log ('express Server Start');
-})
+});
