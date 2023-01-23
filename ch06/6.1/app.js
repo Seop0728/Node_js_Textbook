31410c63114f683aebc8cb168881ba6296f3d1f9
Seop0728 <ms_sh0728@naver.com>
2023-01-23 16:30:03 +0900

    update : morgan,cookeParser Text

--- ch06/6.1/app.js
+++ ch06/6.1/app.js
@@ -1,17 +1,33 @@
 const express = require ('express');
 const path = require ('path');
+const morgan = require ('morgan');
+const cookieParser = require ('cookie-parser');
+
 const app = express ();
 
 app.set ('port', process.env.PORT || 3000);
 
-app.use (( req, res, next ) => {
-	console.log ('1 요청을 실행하고싶어요');
-	next ();
-});
+app.use (morgan ('dev')); // 개발할때 사용
+// app.use(morgan('combined'))  // 배포할때 사용
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use (cookieParser ());
+
+// app.use (( req, res, next ) => {
+// 	console.log ('1 요청을 실행하고싶어요');
+// 	next ();
+// }, ( req, res, next ) => {
+// 	try {
+// 		console.log ("try 에러문");
+// 		console.log (asdffasdasdf);
+// 	} catch ( err ) {
+// 		next (err);
+// 	}
+// });
 
 app.get ('/', ( req, res ) => {
-	// res.sendFile (path.join (__dirname, 'index.html'));
-	res.status(200).send("hello")
+	res.sendFile (path.join (__dirname, 'index.html'));
 });
 
 
@@ -23,9 +39,9 @@ app.get ('/about', ( req, res ) => {
 	res.send ('hello express');
 });
 
-app.use((req, res, next) => {
-	res.send('404에러')
-})
+app.use (( req, res, next ) => {
+	res.status (404).send ('404에러');
+});
 
 app.use (( err, req, res, next ) => {
 	console.log (err);
