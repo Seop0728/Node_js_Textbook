468c8dd2f71dc30fe63761b30a4037ddd9e6fe0f
Seop0728 <ms_sh0728@naver.com>
2023-01-30 19:53:34 +0900

    FEAT : login

--- ch09/controllers/auth.js
+++ ch09/controllers/auth.js
@@ -1,16 +1,36 @@
+const User = require('../models/user');
+const bcrypt = require('bcrypt')
+
 /*
 join.html 에서 /auth/join 으로 POST 요청
-email,name,password 정보가 req.body에 담김
-* */
-exports.join = (req, res, next) => {
-	const {} = req.body
+email,nick,password 정보가 req.body에 담김
+*/
+exports.join = async ( req, res, next ) => {
+	const { email, nick, password } = req.body;
+	try {
+		const exUser = await User.findOne({ where : { email } });
+		if ( exUser ) {
+			return res.redirect('/join?error=exist')
+		}
 
-}
+		const hash = await bcrypt.hash(password,12)
+		await User.create({
+			email,
+			nick,
+			password: hash
+		})
+		return res.redirect('/')
+	} catch ( err ) {
+		console.error(err);
+		next(err);
+	}
+
+};
 
 exports.login = () => {
 
-}
+};
 
 exports.logout = () => {
 
-}
+};
--- ch09/passport/index.js
+++ ch09/passport/index.js
@@ -4,13 +4,17 @@ const kakao = require('./kakaogStrategy');
 const User = require('../models/user');
 
 module.exports = () => {
-	passport.serializeUser(( user, done ) => {
-		done(null, user.id);
-	});
+  passport.serializeUser((user, done) => {
+    console.log('serialize');
+    done(null, user.id);
+  });
 
 	passport.deserializeUser((id,done) => {
 		User.findOne({ where: {id} })
-			.then(( user:User ) => done(null, user))
-			.catch(err => done(err))
+      .then(user => {
+        console.log('user', user);
+        done(null, user);
+       })
+      .catch(err => done(err));
 	})
 };
